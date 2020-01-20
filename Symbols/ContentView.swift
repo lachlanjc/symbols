8fe2d85702d00f43be69c8030be0429bdb646ed8
Lachlan Campbell <lachlan@hackclub.com>
2020-01-20 12:01:42 -0500

    Initial Commit

---
+++ Symbols/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Symbols
+//
+//  Created by Lachlan Campbell on 1/20/20.
+//  Copyright © 2020 Lachlan Campbell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
