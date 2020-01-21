a417445610e85bf0ee6f6ccd4129ac46b16dfcb3
Lachlan Campbell <lachlan@hackclub.com>
2020-01-21 14:46:07 -0500

    Begin app

--- Symbols/ContentView.swift
+++ Symbols/ContentView.swift
@@ -8,9 +8,30 @@
 
 import SwiftUI
 
+struct SymbolRow: View {
+    var name: String
+    var body: some View {
+        HStack {
+            Image(systemName: name).font(.system(size: 32)).padding()
+            Text(name)
+        }
+    }
+}
+
+struct Symbol: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        let symbols = symbolData.map { Symbol(name: $0) }
+        return NavigationView {
+            List(symbols) { symbol in
+                SymbolRow(name: symbol.name)
+            }
+            .navigationBarTitle(Text("Symbols"))
+        }
     }
 }
 
---
+++ Symbols/Data.swift
@@ -0,0 +1,33 @@
+//
+//  Data.swift
+//  Symbols
+//
+//  Created by Lachlan Campbell on 1/20/20.
+//  Copyright © 2020 Lachlan Campbell. All rights reserved.
+//
+
+import Foundation
+
+let symbolData: [String] = load("symbols.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
